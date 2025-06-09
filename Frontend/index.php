989713b44025330d3f5155ec6fd4689d4dafc282
Kettle1703 <115614072+Kettle1703@users.noreply.github.com>
2025-06-09 22:55:05 +0500

    first_test
    Установлены связи: Фрон - Бэк - БД

---
+++ Frontend/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Ввод данных</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="center-box">
+    <label id="output-label">Введите команду</label>
+    <input type="text" id="user-input" name="user_input">
+    <button id="submit-btn">Отправить</button>
+  </div>
+
+  <script src="script.js"></script>
+</body>
+</html>
